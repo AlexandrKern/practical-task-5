41da5a00ecc1eb634e44a90bb579bd35f8172fc9
AlexandrKern <137929102+AlexandrKern@users.noreply.github.com>
2023-07-09 21:21:47 +0300

    Добавьте файлы проекта.

---
+++ practical task 5.1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practical_task_5._1
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Меняет слова местами в обратной последовательности. 
+        /// </summary>
+        /// <param name="inputPhrase"></param>
+        static void ReversWords(string inputPhrase)
+        {
+            string[] revers = SplitText(inputPhrase);
+            Console.WriteLine();
+            for (int i = revers.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{revers[i]} ");
+            }
+        }
+        /// <summary>
+        /// Разделяет строку на слова
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        static string[] SplitText(string text)
+        {
+            string[] arrayOfWords = text.Split();
+            Console.WriteLine();
+            for (int i = 0; i < arrayOfWords.Length; i++)
+            {
+                Console.WriteLine(arrayOfWords[i]);
+            }
+            return arrayOfWords;
+        }
+        static void Main(string[] args)
+        {
+            //Задание 2.Перестановка слов в предложении
+            Console.Write("Введите предложение: ");
+            string inputPhrase = Console.ReadLine();
+            ReversWords(inputPhrase);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ practical task 5/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practical_task_5
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Разделяет строку на слова
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        static string[] SplitText(string text)
+        {
+            int j = 0;
+            string[] arrayOfWords = new string[text.Length];
+            char[] n = new char[text.Length];
+            n = text.ToCharArray();
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (n[i] != ' ') arrayOfWords[j] += n[i].ToString();
+                else j++;
+            }
+            return arrayOfWords;
+        }
+        /// <summary>
+        /// Выводит слова на экран
+        /// </summary>
+        /// <param name="arrayOfWords"></param>
+        static void Print(string[] arrayOfWords)
+        {
+            Console.WriteLine();
+            for (int i = 0; i < arrayOfWords.Length; i++)
+            {
+                if (arrayOfWords[i] != null) Console.WriteLine(arrayOfWords[i]);
+                else break;
+            }
+        }
+        static void Main(string[] args)
+        {
+            //Задание 1.Метод разделения строки на слова
+            Console.Write("Введите предложение: ");
+            string text = Console.ReadLine();
+            string[] arrayOfWords = SplitText(text);
+            Print(arrayOfWords);
+            Console.ReadKey();
+        }
+    }
+}
